18e089bd72d0d6be44c90aede067719eeec058f8
Camibrit <camila-brito10@hotmail.com>
2024-04-10 10:56:47 -0300

    controllers

---
+++ src/main/java/repositories/UserdeptApplication.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdeptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserdeptApplication.class, args);
+    }
+}
